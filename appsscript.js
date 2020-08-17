afc731222a30b22167df944d4b4bdd18ee7f4e09
Kasinath Kottukkal <kasinathkp@gmail.com>
2020-08-17 18:19:50 +0800

    Create appsscript.js

---
+++ appsscript.js
@@ -0,0 +1,14 @@
+{
+  "timeZone": "Asia/Hong_Kong",
+  "dependencies": {
+    "libraries": [{
+      "userSymbol": "OAuth2",
+      "libraryId": "1B7FSrk5Zi6L1rSxxTDgDEUsPzlukDsi4KGuTMorsTQHhGBzBkMun4iDF",
+      "version": "38"
+    }]
+  },
+  "exceptionLogging": "STACKDRIVER",
+  "runtimeVersion": "V8",
+  "chat": {
+  }
+}
